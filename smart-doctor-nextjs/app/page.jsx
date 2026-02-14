ec6b8650605f350a74847a1d3032b48be57aa3a7
Nikhil Raj <129590267+TheNikhilRaj@users.noreply.github.com>
2026-02-14 11:18:28 +0530

    Delete smart-doctor-nextjs directory

--- smart-doctor-nextjs/app/page.jsx
+++
@@ -1,9 +0,0 @@
-'use client';
-
-export default function Home() {
-  return (
-    <div className="flex items-center justify-center min-h-screen">
-      <h1 className="text-4xl font-bold">Smart Doctor App</h1>
-    </div>
-  );
-}
